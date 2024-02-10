0e20e20fd2aff743571c8b08785621736c5c03ec
Jan Obermüller <jan_obermueller@web.de>
2024-02-10 17:56:58 +0100

    connect/disconnect every 10 minutes

--- main.py
+++ main.py
@@ -5,15 +5,66 @@ from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime
 from os import mkdir
 from os.path import exists
+from time import sleep
 
-# prepare data 
-data = {}
+def receiveData():
+  # init data
+  data = {}
 
-def saveData():
+  # prepare timstamp
   now = datetime.now()
   date = now.strftime("%Y%m%d")
   time = now.strftime("%H%M%S")
 
+  def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+      print("Connected to MQTT Broker!")
+    else:
+      print("Failed to connect, return code %d\n", rc)
+
+  def on_disconnect(client, userdata, rc):
+    if rc == 0:
+      print("Disconnected from MQTT Broker!")
+    else:
+      print("Failed to disconnect, return code %d\n", rc)
+
+  def on_message(client, userdata, msg):
+    topic = msg.topic
+    value = msg.payload.decode()
+    data[topic] = value
+
+    # alive print
+    if topic.endswith("/dtu/uptime"):
+      print(f"{datetime.now().isoformat()}: {topic} = {value}")
+
+  # setup mqtt client
+  broker = 'test.mosquitto.org'
+  port = 1883
+  topic = "OpenDTU-A60F1C/#"
+  # Generate a Client ID with the subscribe prefix.
+  client_id = f'subscribe-{randint(0, 100)}'
+
+  client = Client(client_id)
+  client.on_connect = on_connect
+  client.on_disconnect = on_disconnect
+  client.on_message = on_message
+
+  # connect
+  print("connect to broker")
+  client.connect(broker, port)
+  client.loop_start()
+
+  # subscrib and wait
+  client.subscribe(topic)
+  print("wait 30 seconds")
+  sleep(30)
+
+  # disconnect
+  print("disconnect from broker")
+  client.disconnect()
+  client.loop_stop()
+
+  # save data to file
   if not exists(date): mkdir(date)
   with open(date+"/"+time+".txt", "w") as f:
     for topic, value in data.items():
@@ -21,34 +72,9 @@ def saveData():
 
 # prepare scheduler
 scheduler = BackgroundScheduler()
-scheduler.add_job(saveData, CronTrigger(minute="0,10,20,30,40,50"))
+scheduler.add_job(receiveData, CronTrigger(minute="0,10,20,30,40,50"))
+
+# start and wait forever
 scheduler.start()
-
-broker = 'test.mosquitto.org'
-port = 1883
-topic = "OpenDTU-A60F1C/#"
-# Generate a Client ID with the subscribe prefix.
-client_id = f'subscribe-{randint(0, 100)}'
-
-
-def on_connect(client, userdata, flags, rc):
-  if rc == 0:
-    print("Connected to MQTT Broker!")
-  else:
-    print("Failed to connect, return code %d\n", rc)
-
-def on_message(client, userdata, msg):
-  topic = msg.topic
-  value = msg.payload.decode()
-  print(f"{datetime.now().isoformat()}: {topic} = {value}")
-
-  data[topic] = value
-
-
-client = Client(client_id)
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect(broker, port)
-client.subscribe(topic)
-client.loop_forever()
+while True:
+  sleep(1)
